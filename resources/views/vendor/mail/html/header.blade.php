59e4d2891c42aa45e89e24ef6819b643c7df2743
diarra030 <diarraaboubacar030@gmail.com>
2025-12-15 15:25:18 +0000

    ajout de l option Rappel sur un doc

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+<img src="{{ asset('img/5-1.png') }}" class="logo" alt="{{ config('app.name') }}" style="max-width: 100%; border: none; height: 75px; max-height: 75px; width: auto;">
+</a>
+</td>
+</tr>
